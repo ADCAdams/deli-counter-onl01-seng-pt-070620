fad92a0bc8b1d5a7deb6991819e20f9d3bdd00f6
Andy Adams <andrewdcadams@gmail.com>
2020-07-09 02:23:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,14 @@ katz_deli = []
 
 def takeANumber(lineArray,newName)
   lineArray.push(newName);
-  return "Welcome, #{newName}. You are number #{lineArray.length} in line."
+  puts("Welcome, #{newName}. You are number #{lineArray.length} in line.")
 end
 
 def nowServing(lineArray)
   if lineArray.length === 0
-    return "There is nobody waiting to be served!"
+     puts("There is nobody waiting to be served!")
   else
-    return "Currently serving #{lineArray.shift()}."
+    puts("Currently serving #{lineArray.shift()}.")
   end
 end
 
@@ -21,17 +21,16 @@ def line(lineArray)
   bigString = "The line is currently:"
    if  lineArray.length === 0
      puts("The line is currently empty.")
-    return "The line is currently empty."
   else
     i = 0 
     while i < lineArray.length 
       if((lineArray.length - i) === 1)
          bigString = bigString.concat(" #{i + 1}. #{lineArray[i]}");
        else 
-        bigString = bigString.concat(" #{i + 1}. #{lineArray[i]},");
+        bigString = bigString.concat(" #{i + 1}. #{lineArray[i]},")
       end
       i += 1 
     end 
-      return bigString;
+      puts(bigString)
     end
 end
